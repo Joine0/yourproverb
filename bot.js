b86c972e3708ebaa9d73626fc67137b4bfbd6ffd
Joine0 <38402849+Joine0@users.noreply.github.com>
2018-05-15 20:48:33 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -31,11 +31,11 @@ client.on("message", message => {
     }
 });
 
- client.on('message', message => {
+  client.on('message', message => {
             if(!message.channel.guild) return;
 let args = message.content.split(' ').slice(1).join(' ');
 if (message.content.startsWith('R-bc.')){
- if(!message.author.id !== '274923685985386496') return;
+    if(message.author.id != '274923685985386496')  return message.reply('**relly ?**')
 message.channel.sendMessage('جار ارسال الرسالة');
 message.channel.sendMessage(`[ ${client.users.size} / ${client.users.filter(m => m.presence.status !== 'online').size} ] عدد المستلمين `);
 
@@ -44,6 +44,7 @@ m.sendMessage(args)
 })
 }
 });
+
 
 ///restartbot///stopbot
 client.on("message", message => {
